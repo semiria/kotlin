7b866630816a957a5eb531ec045cf53811844c5b
Stephanie Guilbert <sguilbert@salesforce.com>
2020-07-02 17:18:04 -0400

    Add step to build config

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -42,4 +42,26 @@ object Build : BuildType({
         vcs {
         }
     }
+
+    id("Build")
+    steps {
+        script {
+            name = "Set version using script"
+            scriptContent = """
+      #!/bin/bash
+      HASH=%build.vcs.number%
+      SHORT_HASH=${"$"}{HASH:0:7}
+      BUILD_COUNTER=%build.counter%
+      BUILD_NUMBER="1.0${"$"}BUILD_COUNTER.${"$"}SHORT_HASH"
+      echo "##teamcity[buildNumber '${"$"}BUILD_NUMBER']"
+      """.trimIndent()
+        }
+        script {
+            name = "build"
+            scriptContent = """
+      mkdir bin
+      echo "built artifact" > bin/compiled.txt
+      """.trimIndent()
+        }
+    }
 })
